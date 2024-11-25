dbca9c125cf49a38c138b108bf34bfbcd1d418f4
guilherme <guilhermesilva0281@gmail.com>
2024-11-25 19:44:21 -0300

    organizing project structure and creating first user route

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const app = express()
+app.use(express.json())
+
+const routes = require("./routes")
 
 const PORT = 3333
+
+app.use(routes)
 
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
